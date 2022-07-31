d13969ac8cb3bc09da02b2b7fc85e87b583e6e0a
Anıl Bulsun <81930281+anilblsn@users.noreply.github.com>
2022-07-31 07:27:00 +0300

    v1
    v1

--- models/users.js
+++ models/users.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const usersSchema = new Schema({
-  KullaniciAdi: {
+  kullaniciAdi: {
     type: String,
     require: true
   },
